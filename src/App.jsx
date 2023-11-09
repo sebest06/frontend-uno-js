d850e9f91f2cc77fd938e0357849b3af4daf9f06
sebest <sebastianoldani@gmail.com>
2023-11-08 22:17:41 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+import { io } from "socket.io-client";
+
+// "undefined" means the URL will be computed from the `window.location` object
+const URL =
+  process.env.NODE_ENV === "production" ? undefined : "http://localhost:8080";
+
+export const socket = io(URL);
+
+function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value) {
+      setFooEvents((previous) => [...previous, value]);
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("foo", onFooEvent);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("foo", onFooEvent);
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>FrontEnd UNO!</h1>
+      <div className="App">
+        <div>{isConnected}</div>
+        <div>{fooEvents} </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
